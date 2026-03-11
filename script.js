596d7cace8fb3815644662722d71cfea3fb4f307
kiruicompyu <kiruienock2006@gmail.com>
2026-03-11 21:26:01 +0300

    Added a few sections to the page

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+// * about section cards
+const aboutSection = document.getElementById("About")
+// json format for the data to be populated 
+const AboutData = {
+    "set1": {
+        "id": 1
+        
+    }
+}
+// actual population of data
+aboutSection.innerHTML = `
+    // ^ i am going to write it in the html then paste the snippet here.
+
+`
+aboutSection.style
