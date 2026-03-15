037e2230bd836419062195eed692e199c42a4082
kiruicompyu <kiruienock2006@gmail.com>
2026-03-15 13:51:44 +0300

    Added stuff to hero section

--- js/script.js
+++ js/script.js
@@ -1,21 +1,23 @@
 
+let result = confirm("Do you love God?....🫠🙃");
 
-
-
-
+if (result) {
+    alert("I am glad to hear that! Keep loving God and stay blessed.🙏😊");
+} else {
+    alert("Alright let's just continue🙁")
+}
 
 // * about section cards
-const aboutSection = document.getElementById("About")
-// json format for the data to be populated 
-const AboutData = {
-    "set1": {
-        "id": 1
+// const aboutSection = document.getElementById("About")
+// // json format for the data to be populated 
+// const AboutData = {
+//     "set1": {
+//         "id": 1
         
-    }
-}
-// actual population of data
-aboutSection.innerHTML = `
-    // todo: write it in the html then paste the snippet here.
+//     }
+// }
+// // actual population of data
+// aboutSection.innerHTML = `
+//     // todo: write it in the html then paste the snippet here.
 
-`
-aboutSection.style
+// `
